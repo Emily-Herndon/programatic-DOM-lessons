fe47ec601d1845d83c6f711e2bc22558bc4f18c4
weston-bailey <wdbailey87@gmail.com>
2022-04-21 16:01:54 -0700

    _

--- grids/scripts/app.js
+++ grids/scripts/app.js
@@ -1,7 +1,7 @@
 const generatePyramid = num => {
   const pyramid = document.querySelector('#pyramid')
 
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i <= num; i++) {
     const row = document.createElement('div')
     row.classList.add('row')
     for (let j = 0; j < i; j++) {
@@ -16,27 +16,26 @@ const generatePyramid = num => {
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('#grid')
 
-  // for (let i = 0; i < 64; i++) {
-  //   const square = document.createElement('div')
-  //   square.classList.add('square')
-  //    console.log(i % 8)
-  //   const color = i % 2 == 1 ? 'red' : 'black'
-  //   square.classList.add(color)
-  //   container.appendChild(square)
-  // }
+  for (let i = 0; i < 64; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    const color = i % 2 == 1 ? 'red' : 'black'
+    square.classList.add(color)
+    container.appendChild(square)
+  }
   
 
-  // for (let i = 0; i < 8; i++) {
-  //   for (let j = 0; j < 8; j++) {
-  //     const square = document.createElement('div')
-  //     square.classList.add('square')
-  //     const mod = j % 2 == 0 ? 1 : 0
-  //     const color = i % 2 == mod ? 'red' : 'black'
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      const square = document.createElement('div')
+      square.classList.add('square')
+      const mod = j % 2 == 0 ? 1 : 0
+      const color = i % 2 == mod ? 'red' : 'black'
 
-  //     square.classList.add(color)
-  //     container.appendChild(square)
-  //   }
-  // }
+      square.classList.add(color)
+      container.appendChild(square)
+    }
+  }
 
   generatePyramid(16)
 
